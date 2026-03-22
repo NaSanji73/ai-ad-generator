2f4a8f7fae146b4791b1d28f95d56df97d4364b9
NaSanji73 <sanr2706@gmail.com>
2026-03-22 14:50:54 +0530

    fix hashing crash

--- database.py
+++ database.py
@@ -3,6 +3,9 @@ from pymongo import MongoClient
 
 MONGO_URI = os.getenv("MONGO_URI")
 
+if not MONGO_URI:
+    raise Exception("MONGO_URI is not set")
+
 client = MongoClient(MONGO_URI)
 
 db = client["ai_ads"]
