1f17039c5a545d688b06b69fc36e6ae94c8a7605
NaSanji73 <sanr2706@gmail.com>
2026-03-22 12:30:40 +0530

    fixed cors for vercel

--- main.py
+++ main.py
@@ -13,10 +13,7 @@ app = FastAPI(
 # Allow React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001"
-        ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
